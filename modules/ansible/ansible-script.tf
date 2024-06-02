a7c8f012b8e05fa8048da87a26d9acb1a44dfce3
nicmboso <devops.wizzy@gmail.com>
2024-06-02 09:24:38 +0100

    ansible server updates

--- modules/ansible/ansible-script.tf
+++ modules/ansible/ansible-script.tf
@@ -1,23 +1,8 @@
 #Installing Docker Instance
 
 locals {
-  docker_user_data = <<-EOF
+  ansible_user_data = <<-EOF
 #!/bin/bash
-sudo yum update -y
-sudo yum upgrade
-sudo yum install -y yum-utils
-sudo yum config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install docker-ce -y
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo usermod -aG docker ec2-user   
-sudo mkdir /opt/docker
-sudo chown -R ec2-user:ec2-user /opt/
-cd /opt
-sudo chmod 600 /opt/docker
-curl -Ls https://download.newrelic.com/install/newrelic-cli/scripts/install.sh | bash && sudo NEW_RELIC_API_KEY=NRAK-ZTC5BPSBEBNDDQ1AEN6TY2KWOOJ NEW_RELIC_ACCOUNT_ID=4155533 NEW_RELIC_REGION=EU /usr/local/bin/newrelic install -y
-sudo hostnamectl set-hostname Docker
+
 EOF
 }
-
-  
--- modules/ansible/main.tf
+++ modules/ansible/main.tf
@@ -1,17 +1,4 @@
-# # Docker server
-# resource "aws_instance" "docker-server" {
-#   ami                    = var.red_hat
-#   instance_type          = "t2.medium"
-#   subnet_id              = var.docker_subnet
-#   key_name               = var.pub_key
-#   vpc_security_group_ids = [var.docker_sg]
-#   # user_data              = local.docker_user_data
-
-#   tags = {
-#     Name = var.docker_name
-#   }
-# }
-
+# Ansible server
 resource "aws_instance" "ansible-server" {
   ami                         = var.red_hat
   instance_type               = "t2.medium"
