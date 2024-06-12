f92b8b67fd60edc74a5aa837360acfd5b96eb61e
nicmboso <devops.wizzy@gmail.com>
2024-06-12 19:49:00 +0100

    lb-prod updates

--- main.tf
+++ main.tf
@@ -15,6 +15,7 @@ module "vpc" {
   name-prvsn2 = "${local.name}-prvsn2"
   name-pub-rt = "${local.name}-pub-rt"
   name-prv-rt = "${local.name}-prv-rt"
+  
 }
 
 module "securitygroup" {
@@ -137,58 +138,9 @@ module "sonarqube" {
   subnet_id = ""
 }
 
-
-# Creating Load Balancer Target Group for ASG Prod
-resource "aws_lb_target_group" "lb-tg-prod" {
-  name     = "lb-tg-prod"
-  port     = 8080
-  protocol = "HTTP"
-  vpc_id   = module.aws_vpc.vpc_id
-
-  health_check {
-    interval            = 30
-    timeout             = 10
-    healthy_threshold   = 3
-    unhealthy_threshold = 5
-  }
-}
-
-# Creating Application Load Balancer for ASG Prod
-resource "aws_lb" "alb-prod" {
-  name                       = "asg-prod-alb"
-  internal                   = false #makes the lb accessible to the public
-  load_balancer_type         = "application"
-  security_groups            = [aws_security_group.asg-sg.id]
-  subnets                    = [aws_subnet.public-subnet1.id, aws_subnet.public-subnet2.id]
-  enable_deletion_protection = false
-
-  tags = {
-    Name = "${local.name}-alb-prod"
-  }
-}
-
-#Creating Load Balancer Listener for https
-resource "aws_lb_listener" "lb_lsnr-1" {
-  load_balancer_arn = module.aws_lb.alb-prod.arn
-  port              = var.port_https
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = module.aws_acm_certificate.ssl-cert.arn
-
-  default_action {
-    type             = "forward"
-      target_group_arn = module.aws_lb_target.lb-tg-prod.arn
-  }
-}
-
-# Creating Load Balancer Listener for http
-resource "aws_lb_listener" "lb_lsnr-2" {
-  load_balancer_arn = module.aws_lb.alb-prod.arn
-  port              = var.port_http
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = module.aws_lb_target_group.lb-tg-prod.arn
-  }
+module "prod-lb" {
+  source = "./modules/prod-lb"
+  port_http = 80
+  port_https = 443
+  name-alb-prod = "${local.name}-alb-prod"  
 }
---
+++ modules/prod-lb/variable.tf
@@ -0,0 +1,3 @@
+variable "port_http" {}
+variable "port_https" {}
+variable "name-alb-prod" {}
